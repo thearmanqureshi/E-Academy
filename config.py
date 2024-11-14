6ad9a61d9cbfaefc7f7092cffd3b2f8eef31ea77
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-14 11:41:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
-    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/EAcademy')
+    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://thearmanqureshi:Aq-000000@eacademycluster.ffwym.mongodb.net/')

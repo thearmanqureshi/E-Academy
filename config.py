1fedebf6e77bb04c810658d86c9a0e1676843e89
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-14 16:49:48 +0530

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'default_secret_key')
-    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb+srv://thearmanqureshi:Aq-000000@eacademycluster.ffwym.mongodb.net/userLogin?retryWrites=true&w=majority')

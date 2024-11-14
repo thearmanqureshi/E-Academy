ad6b0c2ab0f7efcbcab8e54aff308636c520a04b
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-14 17:19:50 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+from datetime import timedelta
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MONGO_URI = os.environ.get('MONGO_URI')
+    SESSION_COOKIE_SECURE = True
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+    
+class ProductionConfig(Config):
+    DEBUG = False
+    TESTING = False
+    # Production-specific settings
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = 'Lax'
+    
+class DevelopmentConfig(Config):
+    DEBUG = True
+    TESTING = True
+    SESSION_COOKIE_SECURE = False

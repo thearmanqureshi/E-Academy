03603a2f97401470a637dbc1c34fb3c2c335c66a
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-16 12:30:49 +0530

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -104,7 +104,7 @@ const displayItem = (items) => {
               <img src=${image}>
               <h3>${title}</h3>
               <p>${para}</p>
-            <a href="{{ url_for('login') }}">
+            <a href="/login">
               <div class="course-btn">Join Now</div>
             </a>
           </div>`;

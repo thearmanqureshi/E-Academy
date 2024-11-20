0c40a697871b633fbad29b590657709882b77284
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-20 18:19:42 +0530

    Update recaptcha.js

--- static/js/recaptcha.js
+++ static/js/recaptcha.js
@@ -1,7 +1,23 @@
+// Show Hide Password
+document.getElementById("toggle-password-icon").addEventListener("click", function () {
+    const passwordField = document.getElementById("password");
+    const icon = this;
+    if (passwordField.type === "password") {
+      passwordField.type = "text";
+      icon.classList.remove("fa-lock");
+      icon.classList.add("fa-unlock");
+    } else {
+      passwordField.type = "password";
+      icon.classList.remove("fa-unlock");
+      icon.classList.add("fa-lock");
+    }
+});
+
+// Google reCAPTCHA
 function captchaVerified() {
-    document.getElementById('btn').disabled = false;
+  document.getElementById("btn").disabled = false;
 }
 
 function captchaExpired() {
-    document.getElementById('btn').disabled = true;
+  document.getElementById("btn").disabled = true;
 }

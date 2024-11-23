e5e757a2c3a296b847dd946382b31a7a803bde03
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-23 17:21:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, request, abort, session, jsonify
+from flask import Flask, render_template, redirect, url_for, flash, request, abort, session, jsonify, make_response
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user

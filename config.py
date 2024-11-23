e19f7c522611d6185ee2ea7a19bfb5e97db28b3b
Arman Qureshi <thearmanqureshi@gmail.com>
2024-11-23 15:56:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,16 +3,22 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    MONGO_URI = os.environ.get('MONGO_URI')
+    MONGO_AUTH_URL = os.environ.get('MONGO_AUTH_URL')
     MONGO_COURSE_URL = os.environ.get('MONGO_COURSE_URL')
+    MONGO_PREDICTION_URL = os.environ.get('MONGO_PREDICTION_URL')
     RECAPTCHA_SECRET_KEY = os.environ.get('RECAPTCHA_SECRET_KEY')
+    MODEL_URL = os.getenv('MODEL_URL')
+    SCALER_URL = os.getenv('SCALER_URL')
     SESSION_COOKIE_SECURE = True
     PERMANENT_SESSION_LIFETIME = timedelta(minutes=30)
+    MAX_PREDICTION_SCORE = 98
+    MAX_AGE = 100
+    MAX_STUDY_HOURS = 24
+    MAX_FAILURES = 10
     
 class ProductionConfig(Config):
     DEBUG = False
     TESTING = False
-    # Production-specific settings
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_SAMESITE = 'Lax'
     
